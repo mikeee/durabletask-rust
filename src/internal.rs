96185bbe7e8587cad8a2f80c9a4e37a7cc386a69
mikeee <hey@mike.ee>
2024-04-26 16:03:14 +0100

    feat: internal task func name helper
    Signed-off-by: mikeee <hey@mike.ee>

--- src/internal.rs
+++ src/internal.rs
@@ -1,6 +1,21 @@
 use gethostname::gethostname;
 use uuid::Uuid;
 
+/// Return the function name as a String
+pub(crate) fn get_task_function_name<F>(_: F) -> String {
+    let function_path = std::any::type_name::<F>();
+    // Return the function name without the preceding path
+    function_path.split("::").last().unwrap().to_string()
+}
+
+/// Return the default worker name consisting of:
+///
+/// - Hostname
+/// - Process ID (PID)
+/// - Universally Unique Identifier v4 (UUID)
+///
+/// Returned as a String in this format:
+/// {Hostname},{PID},{UUID}
 pub(crate) fn get_default_worker_name() -> String {
     let hostname: String = gethostname().into_string().unwrap_or("unknown".to_string());
     let pid = std::process::id();
@@ -12,6 +27,17 @@ pub(crate) fn get_default_worker_name() -> String {
 mod tests {
     use crate::internal::get_default_worker_name;
     use uuid::Uuid;
+
+    use super::get_task_function_name;
+
+    #[test]
+    fn test_get_task_function_name() {
+        fn test_task_function(output: String) {
+            println!("finished something: {output}")
+        }
+        let result = get_task_function_name(test_task_function);
+        assert_eq!(result, "test_task_function")
+    }
 
     #[test]
     fn test_get_default_worker_name() {
